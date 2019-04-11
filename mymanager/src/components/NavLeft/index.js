d700e4d6f9c193fa0b5449d04d52e21cbe57f30e
huoran <git@huoran.com>
2019-04-11 22:46:22 +0800

    菜单管理

--- mymanager/src/components/NavLeft/index.js
+++ mymanager/src/components/NavLeft/index.js
@@ -1,28 +1,47 @@
-import './index.less'
-import MenuConfig from './../../config/menuConfig'
+import "./index.less";
+import MenuConfig from "./../../config/menuConfig";
 
-import React from 'react'
-import {Menu,Icon} from 'antd'
-const SubMenu = Menu.SubMenu
+import React from "react";
+import { Menu, Icon } from "antd";
+const SubMenu = Menu.SubMenu;
 
 export default class NavLeft extends React.Component {
+  componentWillMount() {
+    const menuTreeNode = this.renderMenu(MenuConfig);
 
-    render() {
+    this.setState({
+      menuTreeNode
+    });
+  }
+
+  //菜单渲染
+  renderMenu = data => {
+    return data.map(item => {
+      if (item.children) {
+        //如果children下面还有列表的话，继续循环遍历
         return (
-            <div>
-                <div className="logo">
-                    <img src='/assets/logo-ant.svg' alt='' />
-                    <h1>huoran </h1>
-                </div>
-                <Menu theme='dark'>
-                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                        <Menu.Item key="1">Option 1</Menu.Item>
-                        <Menu.Item key="2">Option 2</Menu.Item>
-                        <Menu.Item key="3">Option 3</Menu.Item>
-                        <Menu.Item key="4">Option 4</Menu.Item>
-                    </SubMenu>
-                </Menu>
-            </div>
-        )
-    }
+          <SubMenu title={item.title} key={item.key}>
+            {this.renderMenu(item.children)}
+          </SubMenu>
+        );
+      }
+      return (
+        <Menu.Item title={item.title} key={item.key}>
+          {item.title}
+        </Menu.Item>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="logo">
+          <img src="/assets/logo-ant.svg" alt="" />
+          <h1>huoran </h1>
+        </div>
+        <Menu theme="dark">{this.state.menuTreeNode}</Menu>
+      </div>
+    );
+  }
 }
