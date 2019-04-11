2a22736e719a41217d04d6b19505eb1ee03c4033
霍然 <303176152@qq.com>
2019-04-11 10:31:17 +0800

    组件生命周期

--- mymanager/src/pages/demo/Child.js
+++ mymanager/src/pages/demo/Child.js
@@ -33,9 +33,11 @@ export default class Child extends React.Component {
     }
 
     render() {
-        return <div>
-            <p>这里是子组件，测试子组件的生命周期</p>
-            <p>{this.props.name}</p>
-        </div>
+        return (
+            <div>
+                <p>这里是子组件，测试子组件的生命周期</p>
+                <p>{this.props.name}</p>
+            </div>
+        )
     }
 }
--- mymanager/src/pages/demo/Life.js
+++ mymanager/src/pages/demo/Life.js
@@ -14,11 +14,14 @@ export default class Life extends React.Component {
     }
 
     render() {
-        return <div style={{ padding: 30 }}>     {/* 样式要有两个{}括号 */} 
-            <p>React生命周期</p>
-            <button onClick={this.handleAdd}>点击一下</button>
-            <p>{this.state.count}</p>
-            <Child name={this.state.count}></Child>
-        </div>
+        return (
+            //样式要有两个{}括号
+            <div style={{ padding: 30 }}>  
+                <p>React生命周期</p>
+                <button onClick={this.handleAdd}>点击一下</button>
+                <p>{this.state.count}</p>
+                <Child name={this.state.count}></Child>
+            </div>
+        )
     }
 }
