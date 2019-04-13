82b0983fbedb1d590f14ac5ade08664a5b839e95
huoran <git@huoran.com>
2019-04-14 07:00:34 +0800

    全局提示框message antd

--- mymanager/src/router.js
+++ mymanager/src/router.js
@@ -8,6 +8,7 @@ import Buttons from "./pages/ui/buttons";
 import Modals from "./pages/ui/modals";
 import Loadings from "./pages/ui/loadings";
 import Notice from "./pages/ui/notice";
+import Messages from "./pages/ui/messages";
 import NoMatch from "./pages/nomatch";
 
 export default class IRouter extends React.Component {
@@ -27,6 +28,7 @@ export default class IRouter extends React.Component {
                   <Route path="/admin/ui/modals" component={Modals} />
                   <Route path="/admin/ui/loadings" component={Loadings} />
                   <Route path="/admin/ui/notification" component={Notice} />
+                  <Route path="/admin/ui/messages" component={Messages} />
                   <Route component={NoMatch} />
                 </Switch>
               </Admin>
